886eb694473d0ccc2e0f1b707059580372b83628
Mark F <exactmark@gmail.com>
2020-12-09 16:55:51 -0500

    Day05 Formatted
    I should actually format my code.

--- Day05/Day05.go
+++ Day05/Day05.go
@@ -6,11 +6,11 @@ import (
 	"strconv"
 )
 
-type Ticket struct{
+type Ticket struct {
 	ticketString string
-	rowNumber int
-	seatNumber int
-	seatID int
+	rowNumber    int
+	seatNumber   int
+	seatID       int
 }
 
 func convTicketPartToInt(inputString string) int {
@@ -31,11 +31,11 @@ func convTicketPartToInt(inputString string) int {
 	return int(i)
 }
 
-func createTicket(ticketString string)Ticket{
+func createTicket(ticketString string) Ticket {
 	rowLetters := ticketString[0:7]
 	seatLetters := ticketString[7:10]
 	rowNumber := convTicketPartToInt(rowLetters)
-	seatNumber:=convTicketPartToInt(seatLetters)
+	seatNumber := convTicketPartToInt(seatLetters)
 	seatID := rowNumber*8 + seatNumber
 	return Ticket{
 		ticketString: ticketString,
@@ -48,42 +48,42 @@ func createTicket(ticketString string)Ticket{
 
 //make a sort!
 type BySeatId []Ticket
+
 func (a BySeatId) Len() int           { return len(a) }
 func (a BySeatId) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a BySeatId) Less(i, j int) bool { return a[i].seatID < a[j].seatID }
 
-
 func solvePt1(inputLines []string) {
 
-	ticketList:=make(map[string]Ticket,0)
-	highestId:=0
-	for _,singleTicket := range inputLines{
-		thisTicket:=createTicket(singleTicket)
-		if thisTicket.seatID>highestId{
-			highestId=thisTicket.seatID
+	ticketList := make(map[string]Ticket, 0)
+	highestId := 0
+	for _, singleTicket := range inputLines {
+		thisTicket := createTicket(singleTicket)
+		if thisTicket.seatID > highestId {
+			highestId = thisTicket.seatID
 		}
-		ticketList[singleTicket]=thisTicket
+		ticketList[singleTicket] = thisTicket
 	}
 
-	fmt.Printf("Highest ticket id is %v\n",highestId)
+	fmt.Printf("Highest ticket id is %v\n", highestId)
 }
 
-func solvePt2(inputLines[]string){
-	ticketList:=make([]Ticket,0)
-	for _,singleTicket := range inputLines{
-		thisTicket:=createTicket(singleTicket)
-		ticketList= append(ticketList,thisTicket)
+func solvePt2(inputLines []string) {
+	ticketList := make([]Ticket, 0)
+	for _, singleTicket := range inputLines {
+		thisTicket := createTicket(singleTicket)
+		ticketList = append(ticketList, thisTicket)
 	}
 	sort.Sort(BySeatId(ticketList))
 	currentTicket := ticketList[0].seatID
-	for _,singleTicket := range ticketList{
-		if singleTicket.seatID == currentTicket{
+	for _, singleTicket := range ticketList {
+		if singleTicket.seatID == currentTicket {
 			currentTicket++
-		}else {
+		} else {
 			break
 		}
 	}
-	fmt.Printf("My ticket is %v\n",currentTicket)
+	fmt.Printf("My ticket is %v\n", currentTicket)
 
 }
 
